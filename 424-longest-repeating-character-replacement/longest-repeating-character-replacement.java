862ee265e6f7374eb7771c55ddfc348aea1566b1
Bibek <bibekbehera283@gmail.com>
2026-07-24 14:26:27 +0530

    Time: 9 ms (59.02%) | Memory: 46.3 MB (38.76%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,32 +1,19 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n = s.length();
-        int c[] = new int[26];
-        char a[] = s.toCharArray();
+        int[] count = new int[26];
+        char[] a = s.toCharArray();
         int low = 0;
-        int res = Integer.MIN_VALUE;
-        for(int high = 0; high < n; high++) {
-            c[a[high] - 65]++;
-            int len = high - low + 1;
-            int max = findMax(c);
-            int diff = len - max;
-            while(diff > k) {
-                c[a[low] - 65]--;
+        int maxFreq = 0;
+        int res = 0;
+        for (int high = 0; high < s.length(); high++) {
+            count[a[high] - 'A']++;
+            maxFreq = Math.max(maxFreq, count[a[high] - 'A']);
+            if ((high - low + 1) - maxFreq > k) {
+                count[a[low] - 'A']--;
                 low++;
-                len = high - low + 1;
-                max = findMax(c);
-                diff = len - max;
             }
-            res = Math.max(res,high - low + 1);
+            res = Math.max(res, high - low + 1);
         }
         return res;
-    }
-
-    public static int findMax(int[] c) {
-        int max = Integer.MIN_VALUE;
-        for(int i : c) {
-            max = Math.max(max,i);
-        }
-        return max;
     }
 }

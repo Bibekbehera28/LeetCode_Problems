445a26cbf55bc71eaa85ed1a2b68c18601ca0157
Bibek <bibekbehera283@gmail.com>
2026-08-19 16:07:15 +0530

    Time: 9 ms (85.26%) | Memory: 46.9 MB (94.83%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        Integer idx[] = new Integer[n];
+        for(int i = 0; i < n; i++) idx[i] = i;
+        Arrays.sort(idx,(a,b) -> {
+            return Integer.compare(heights[b],heights[a]);
+        });
+        String res[] = new String[n];
+        for(int i = 0; i < n; i++) res[i] = names[idx[i]];
+        return res;
+    }
+}
